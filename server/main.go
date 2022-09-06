d54c965a8aa5f52ab56263ee6d54beaabbc22e83
Yobol <m15152036579@163.com>
2022-09-06 23:51:22 +0800

    dev: server - add login api

--- server/main.go
+++ server/main.go
@@ -6,12 +6,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	AuthCookieName = "YAC"
+)
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	route := gin.Default()
+	route.GET("/home", CheckCookie(), func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"data": "Hello",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	apiV1 := route.Group("/api/v1")
+	apiV1.POST("/auth/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		if username != "admin" || password != "123456" { // fake
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		c.SetCookie(AuthCookieName, "ok", 3*24*60*60, "/", "", false, true) // YAS indicates 'Yobol Authentication Cookie'
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Login Success!",
+		})
+	})
+	route.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func CheckCookie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if cookie, err := c.Cookie(AuthCookieName); err != nil {
+			if cookie == "ok" {
+				c.Next()
+				return
+			}
+		}
+
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 }
--- ui/src/api/auth.ts
+++ ui/src/api/auth.ts
@@ -3,7 +3,7 @@ import service from '@/utils/request'
 // @Param data {username: "string", password: "string", remember: "bool"}
 export const login = (data: any) => {
     return service({
-        url: '/api/v1/auth/login',
+        url: 'http://localhost:8080/api/v1/auth/login',
         method: 'post',
         data: data
     })
