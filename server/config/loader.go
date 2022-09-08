063060e9616477bce8e34ae14952b97933af2331
Yobol <m15152036579@163.com>
2022-09-08 22:41:14 +0800

    dev: server - viper init

--- server/config/loader.go
+++ server/config/loader.go
@@ -1,14 +1,28 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+var Config *Server
 
 // environment variables > config defaults
 
 func Init() {
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("yaml")   // REQUIRED if the config does not have the extension in the name
+	v := viper.New()
+	v.SetConfigName("config") // name of config file (without extension)
+	v.SetConfigType("yaml")   // REQUIRED if the config does not have the extension in the name
 
-	viper.AddConfigPath("./etc/")
-	viper.AddConfigPath("$HOME/.yobol/server/")
-	viper.AddConfigPath("/etc/yobol/server/")
+	v.AddConfigPath("./etc/")
+	v.AddConfigPath("$HOME/.yobol/server/")
+	v.AddConfigPath("/etc/yobol/server/")
+
+	if err := v.ReadConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error read config file: %s\n", err))
+	}
+	if err := v.Unmarshal(Config); err != nil {
+		panic(fmt.Errorf("Fatal error unmarshal config file: %s\n", err))
+	}
 }
